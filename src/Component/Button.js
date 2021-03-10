876e22408541c5b3dea28abf37dbdb58c14db1d3
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-03-10 10:58:07 -0400

    Change button to real cards

--- src/Component/Button.js
+++ src/Component/Button.js
@@ -1,7 +1,47 @@
 // import {useState} from 'react'
 
-const Button = (props) => {
-  return <button onClick={props.onClick} value={props.value}>Card{props.value}</button>
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function Button(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} onClick={props.onClick} >
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Card{props.value}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+  );
 }
 
-export default Button;
+
+// const Button = (props) => {
+//   return <button onClick={props.onClick} value={props.value}>Card{props.value}</button>
+// }
+
+// export default Button;
